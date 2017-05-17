ceb0897a6830346be8967c42c8e780987eee6d6a
Sam Davies <sam@razeware.com>
2017-05-17 17:59:52 +0100

    Phase 4: Adding the Speakers model to the data model

--- Server/contact_pb2.py
+++ Server/contact_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='contact.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n\rcontact.proto\"\xdb\x01\n\x07\x43ontact\x12\x12\n\nfirst_name\x18\x01 \x01(\t\x12\x11\n\tlast_name\x18\x02 \x01(\t\x12\x14\n\x0ctwitter_name\x18\x03 \x01(\t\x12\r\n\x05\x65mail\x18\x04 \x01(\t\x12\x13\n\x0bgithub_link\x18\x05 \x01(\t\x12\"\n\x04type\x18\x06 \x01(\x0e\x32\x14.Contact.ContactType\x12\x11\n\timageName\x18\x07 \x01(\t\"8\n\x0b\x43ontactType\x12\x0b\n\x07SPEAKER\x10\x00\x12\r\n\tATTENDANT\x10\x01\x12\r\n\tVOLUNTEER\x10\x02\x62\x06proto3')
+  serialized_pb=_b('\n\rcontact.proto\"\xdb\x01\n\x07\x43ontact\x12\x12\n\nfirst_name\x18\x01 \x01(\t\x12\x11\n\tlast_name\x18\x02 \x01(\t\x12\x14\n\x0ctwitter_name\x18\x03 \x01(\t\x12\r\n\x05\x65mail\x18\x04 \x01(\t\x12\x13\n\x0bgithub_link\x18\x05 \x01(\t\x12\"\n\x04type\x18\x06 \x01(\x0e\x32\x14.Contact.ContactType\x12\x11\n\timageName\x18\x07 \x01(\t\"8\n\x0b\x43ontactType\x12\x0b\n\x07SPEAKER\x10\x00\x12\r\n\tATTENDANT\x10\x01\x12\r\n\tVOLUNTEER\x10\x02\"&\n\x08Speakers\x12\x1a\n\x08\x63ontacts\x18\x01 \x03(\x0b\x32\x08.Contactb\x06proto3')
 )
 
 
@@ -124,9 +124,42 @@ _CONTACT = _descriptor.Descriptor(
   serialized_end=237,
 )
 
+
+_SPEAKERS = _descriptor.Descriptor(
+  name='Speakers',
+  full_name='Speakers',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='contacts', full_name='Speakers.contacts', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=239,
+  serialized_end=277,
+)
+
 _CONTACT.fields_by_name['type'].enum_type = _CONTACT_CONTACTTYPE
 _CONTACT_CONTACTTYPE.containing_type = _CONTACT
+_SPEAKERS.fields_by_name['contacts'].message_type = _CONTACT
 DESCRIPTOR.message_types_by_name['Contact'] = _CONTACT
+DESCRIPTOR.message_types_by_name['Speakers'] = _SPEAKERS
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 Contact = _reflection.GeneratedProtocolMessageType('Contact', (_message.Message,), dict(
@@ -135,6 +168,13 @@ Contact = _reflection.GeneratedProtocolMessageType('Contact', (_message.Message,
   # @@protoc_insertion_point(class_scope:Contact)
   ))
 _sym_db.RegisterMessage(Contact)
+
+Speakers = _reflection.GeneratedProtocolMessageType('Speakers', (_message.Message,), dict(
+  DESCRIPTOR = _SPEAKERS,
+  __module__ = 'contact_pb2'
+  # @@protoc_insertion_point(class_scope:Speakers)
+  ))
+_sym_db.RegisterMessage(Speakers)
 
 
 # @@protoc_insertion_point(module_scope)
